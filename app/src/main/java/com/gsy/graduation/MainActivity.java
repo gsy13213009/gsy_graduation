f2180480178454fd0b4d2815e4be4f5be9128ee9
郭思义 <gsy@meitu.com>
2017-04-19 00:28:52 +0800

    [需求] 添加附近电影院的搜索功能

--- app/src/main/java/com/gsy/graduation/MainActivity.java
+++ app/src/main/java/com/gsy/graduation/MainActivity.java
@@ -3,6 +3,7 @@ package com.gsy.graduation;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -13,17 +14,29 @@ import android.widget.TextView;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CircleOptions;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.gsy.graduation.adapter.MenuListAdapter;
 import com.gsy.graduation.application.BaseApplication;
 import com.gsy.graduation.data.HotMovieData;
 import com.gsy.graduation.utils.DeviceUtils;
+import com.gsy.graduation.utils.ToastUtil;
 import com.gsy.graduation.view.SwitchImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener, PoiSearch.OnPoiSearchListener {
 
     private static final int DURATION_SHOW_MENU = 300;
     private View mMapMenuLl;
@@ -41,6 +54,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private View mMapNight;
     private View mMapNavigate;
     private View mMapModelMenu;
+    private View mMovieAround;
+    // 查询
+    private PoiSearch.Query query;// Poi查询条件类
+    private int currentPage = 0;// 当前页面，从0开始计数
+    private PoiResult poiResult; // poi返回的结果
+    private List<PoiItem> poiItems;// poi数据
+    //    private RelativeLayout mPoiDetail;
+    private Marker mlastMarker;
+    private myPoiOverlay poiOverlay;// poi图层
+    private LatLonPoint lp;// 116.472995,39.993743
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +89,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mMapShadowFl.setOnClickListener(this);
         mMenuSw1.setOnClickListener(this);
         mMenuSw2.setOnClickListener(this);
+        mMovieAround.setOnClickListener(this);
     }
 
     private void initData() {
@@ -91,6 +116,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         mMMenuListView = (ListView) findViewById(R.id.activity_menu_list_view);
         mMenuSw1 = (SwitchImageView) findViewById(R.id.activity_menu_sw1);
         mMenuSw2 = (SwitchImageView) findViewById(R.id.activity_menu_sw2);
+        mMovieAround = findViewById(R.id.activity_map_movie_around);
         findViewById(R.id.activity_main_current_index).setOnClickListener(new LocationIndex());
 
         mMapModelLl.setTranslationY(-DeviceUtils.dip2px(100));
@@ -150,6 +176,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     /**
      * 设置地图模式的显示和隐藏
+     *
      * @param visible true 显示，false 隐藏
      */
     private void setMapModelVisibility(final boolean visible) {
@@ -257,9 +284,29 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 setMapModelVisibility(mMapShadowFl.getVisibility() != View.VISIBLE);
                 break;
 
+            case R.id.activity_map_movie_around:
+                doSearchQuery();
+                break;
+
             default:
                 break;
         }
+    }
+
+    /**
+     * 开始进行poi搜索
+     */
+    protected void doSearchQuery() {
+        currentPage = 0;
+        query = new PoiSearch.Query("电影院", "", "");// 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
+        query.setPageSize(20);// 设置每页最多返回多少条poiitem
+        query.setPageNum(currentPage);// 设置查第一页
+        lp = new LatLonPoint(mAMap.getCameraPosition().target.latitude, mAMap.getCameraPosition().target.longitude);
+        PoiSearch poiSearch = new PoiSearch(this, query);
+        poiSearch.setOnPoiSearchListener(this);
+        poiSearch.setBound(new PoiSearch.SearchBound(lp, 5000, true));//
+        // 设置搜索区域为以lp点为圆心，其周围5000米范围
+        poiSearch.searchPOIAsyn();// 异步搜索
     }
 
     public void getMoviesData() {
@@ -270,6 +317,95 @@ public class MainActivity extends Activity implements View.OnClickListener {
             hotMovieData.setTime("2月14日22:00--24:00");
             mMovieList.add(hotMovieData);
         }
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult result, int rcode) {
+        if (rcode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                if (result.getQuery().equals(query)) {// 是否是同一条
+                    poiResult = result;
+                    poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
+                    List<SuggestionCity> suggestionCities = poiResult
+                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
+                    if (poiItems != null && poiItems.size() > 0) {
+                        //并还原点击marker样式
+                        if (mlastMarker != null) {
+                            resetlastmarker();
+                        }
+                        //清理之前搜索结果的marker
+                        if (poiOverlay != null) {
+                            poiOverlay.removeFromMap();
+                        }
+                        mAMap.clear();
+                        poiOverlay = new myPoiOverlay(mAMap, poiItems, MainActivity.this);
+                        poiOverlay.addToMap();
+                        poiOverlay.zoomToSpan();
+
+                        mAMap.addMarker(new MarkerOptions()
+                                .anchor(0.5f, 0.5f)
+                                .icon(BitmapDescriptorFactory
+                                        .fromBitmap(BitmapFactory.decodeResource(
+                                                getResources(), R.drawable.point4)))
+                                .position(new LatLng(lp.getLatitude(), lp.getLongitude())));
+
+                        mAMap.addCircle(new CircleOptions()
+                                .center(new LatLng(lp.getLatitude(),
+                                        lp.getLongitude())).radius(5000)
+                                .strokeColor(Color.BLUE)
+                                .fillColor(Color.argb(50, 1, 1, 1))
+                                .strokeWidth(2));
+
+                    } else if (suggestionCities != null
+                            && suggestionCities.size() > 0) {
+                        showSuggestCity(suggestionCities);
+                    } else {
+                        ToastUtil.show(MainActivity.this,
+                                R.string.no_result);
+                    }
+                }
+            } else {
+                ToastUtil
+                        .show(MainActivity.this, R.string.no_result);
+            }
+        } else {
+            ToastUtil.showerror(this.getApplicationContext(), rcode);
+        }
+    }
+
+    // 将之前被点击的marker置为原来的状态
+    private void resetlastmarker() {
+        int index = poiOverlay.getPoiIndex(mlastMarker);
+        if (index < 10) {
+            mlastMarker.setIcon(BitmapDescriptorFactory
+                    .fromBitmap(BitmapFactory.decodeResource(
+                            getResources(),
+                            poiOverlay.markers[index])));
+        } else {
+            mlastMarker.setIcon(BitmapDescriptorFactory.fromBitmap(
+                    BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight)));
+        }
+        mlastMarker = null;
+
+    }
+
+    /**
+     * poi没有搜索到数据，返回一些推荐城市的信息
+     */
+    private void showSuggestCity(List<SuggestionCity> cities) {
+        String infomation = "推荐城市\n";
+        for (int i = 0; i < cities.size(); i++) {
+            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
+                    + cities.get(i).getCityCode() + "城市编码:"
+                    + cities.get(i).getAdCode() + "\n";
+        }
+        ToastUtil.show(this, infomation);
+
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
     }
 
     private class LocationIndex implements View.OnClickListener {
